02221af88203ee3678e895c37620c8caceded3b9
0xpr4bin <0xpr4bin00gmail.com>
2025-05-23 22:37:10 +0545

    skdhjksa

---
+++ public/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create the directory
+mkdir -p public/blog-images
+
+# Download the images (you'll need to run this on your local machine)
+curl -o public/blog-images/vulnet-enum.jpg "https://prabinsigdel.com.np/images/enum.jpg"
+curl -o public/blog-images/vulnet-nmap-all.jpg "https://prabinsigdel.com.np/images/nmap_all.jpg"
+curl -o public/blog-images/vulnet-redis.jpg "https://prabinsigdel.com.np/images/redis.jpg"
+curl -o public/blog-images/vulnet-redis-exp.jpg "https://prabinsigdel.com.np/images/redis_exp.jpg"
+curl -o public/blog-images/vulnet-responder.jpg "https://prabinsigdel.com.np/images/responder.jpg"
+curl -o public/blog-images/vulnet-smbmap.jpg "https://prabinsigdel.com.np/images/smbmap.jpg"
+curl -o public/blog-images/vulnet-smbclient.jpg "https://prabinsigdel.com.np/images/smbclient.jpg"
+curl -o public/blog-images/vulnet-shell.jpg "https://prabinsigdel.com.np/images/shell.jpg"
+curl -o public/blog-images/vulnet-bloodhound.jpg "https://prabinsigdel.com.np/images/bloodhound.jpg"
